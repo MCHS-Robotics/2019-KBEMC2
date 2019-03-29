0a4521c18e97b105a5eaec8829cebd9bd67450d8
ColeTrammer <cole.trammer@gmail.com>
2019-03-28 18:56:57 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Auto", group="KBEMC2")
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        // init code
+
+        waitForStart();
+
+        // auto
+    }
+}
