e40b5d10f1913ead5f263af8c7c325c1179a3223
ColeTrammer <cole.trammer@gmail.com>
2019-04-04 19:13:26 -0700

    update to collection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hal/Collection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hal/Collection.java
@@ -1,15 +1,42 @@
 package org.firstinspires.ftc.teamcode.hal;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.teamcode.Utilities;
 
 public class Collection {
-    public Collection(Utilities utilities) {}
+    private final DcMotor collection;
+    public Collection(Utilities utilities) {
+        collection = utilities.getHardwareMap().get(DcMotor.class, "collection");
+    }
 
     public void collect() {
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                collection.setPower(1);
+                try {
+                    wait(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                collection.setPower(0);
+            }
+        }).start();
     }
 
     public void release() {
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                collection.setPower(-1);
+                try {
+                    wait(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                collection.setPower(0);
+            }
+        }).start();
     }
 }
