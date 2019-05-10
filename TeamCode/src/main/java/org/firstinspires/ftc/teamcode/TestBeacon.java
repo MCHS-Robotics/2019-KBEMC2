d7b7deb0202ddde863966ba534bc5e5b49913da6
ColeTrammer <cole.trammer@gmail.com>
2019-05-09 20:49:50 -0700

    did stuff again

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBeacon.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hal.Collection;
+import org.firstinspires.ftc.teamcode.hal.ComputerVision;
+import org.firstinspires.ftc.teamcode.hal.Drive;
+
+@TeleOp(name="Test Beacon", group="test")
+public class TestBeacon extends LinearOpMode {
+
+    private ElapsedTime elapsedTime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        Utilities utilities = new Utilities(this, telemetry, hardwareMap, elapsedTime);
+
+
+        ComputerVision computerVision = new ComputerVision(utilities);
+        Collection collection = null;
+        Drive drive = new Drive(utilities);
+        // Lift lift = new Lift(utilities);
+
+        BeaconDeterminer beaconDeterminer = new BeaconDeterminer(utilities);
+
+        waitForStart();
+        elapsedTime.reset();
+
+        BeaconDeterminer.BeaconState beaconState = beaconDeterminer.determine();
+        while (beaconState == BeaconDeterminer.BeaconState.CONFUSED) {
+            beaconState = beaconDeterminer.determine();
+        }
+
+        telemetry.addData("Beacon State", beaconState.toString());
+        telemetry.update();
+
+
+    }
+}
